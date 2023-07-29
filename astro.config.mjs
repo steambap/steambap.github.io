20efade825dd50d14a445e67b6f59ba690b45c5d
Weilin Shi <934587911@qq.com>
2023-07-29 16:28:27 +0800

    add view transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://weilinshi.org',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			langs: ['js', 'ts']
-		},
-	},
+  site: "http://weilinshi.org",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      langs: ["js", "ts"],
+    },
+  },
+  experimental: {
+    viewTransitions: true,
+  },
 });
